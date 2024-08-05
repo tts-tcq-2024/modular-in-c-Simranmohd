c3d7a23b179e1d0e128d0af60445827361094dc0
Simranmohd <120707251+Simranmohd@users.noreply.github.com>
2024-08-05 17:18:53 +0530

    Update color_mapping.h

--- color_mapping.h
+++ color_mapping.h
@@ -1,1 +1,10 @@
+#ifndef COLOR_MAPPING_H
+#define COLOR_MAPPING_H
+
+extern const char* majorColorNames[];
+extern const char* minorColorNames[];
+extern const int numberOfMajorColors;
+extern const int numberOfMinorColors;
+
+#endif // COLOR_MAPPING_H
 
